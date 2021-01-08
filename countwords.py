4fa93bec693e5202f7d81e5629592b7394e5c864
Nithya-m99 <77161846+Nithya-m99@users.noreply.github.com>
2021-01-08 21:37:45 +0530

    Update countwords.py

--- countwords.py
+++ countwords.py
@@ -1,10 +1,10 @@
 
-
+'Total words'
 f=open('t8.shakespeare.txt','r+')
 for i in f:
     print(i)
 
-'Unique count'
+'Uniqueword count'
 f=open('t8.shakespeare.txt','r+')
 lines=f.readlines()
 n=[]
@@ -20,6 +20,13 @@ for k in u:
     c=c+1
 print(c) 
 
+'Process time'
+etime=time.time()-stime
+print("\n")
+print("",etime,"Sec")
+print("\n")
+print(process.memory_info()[0],"Bytes")
+print(process.memory_info()[0]/float(2**20),"MB")
 
 
 
